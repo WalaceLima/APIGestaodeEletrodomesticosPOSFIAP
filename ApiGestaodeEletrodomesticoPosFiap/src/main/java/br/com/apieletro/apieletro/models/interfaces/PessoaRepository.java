10d1bf19e8852cb384fc4e2218ef28a78dc2428e
Walace Lima <weclima@gmail.com>
2023-08-06 12:57:11 -0300

    Criação e ajustes no controller eletroméstico.Criação do controller Pessoa,dto pessoa, e Api para testes OpenApi.Inclusão de depencia para swagger.

--- ApiGestaodeEletrodomesticoPosFiap/src/main/java/br/com/apieletro/apieletro/models/interfaces/PessoaRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.apieletro.apieletro.models.interfaces;
-
-import br.com.apieletro.apieletro.models.Eletrodomestico;
-import br.com.apieletro.apieletro.models.Pessoa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PessoaRepository extends JpaRepository<Pessoa,Long> {}
